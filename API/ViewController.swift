19e1e978d05811dc1d634809c32c1071784dd742
Tamara <tamara.erlij@gmail.com>
2020-05-13 01:57:12 -0300

    Design da tela feita

--- API/ViewController.swift
+++ API/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityTextField: UITextField!
+   
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func submit(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let url = URL(string: "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=439d4b804bc8187953eb36d2a8c26a02")!
@@ -24,6 +30,10 @@ class ViewController: UIViewController {
                     let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as  AnyObject
                         print(jsonResult)
                         print(jsonResult["name"])
+                        
+                        if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String {
+                            print(description)
+                        }
                     } catch {
                         print("JSON Processing Failed")
                     }
