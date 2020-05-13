08a8a9de5eea240f44a136c8b54e8b0fced0d029
Tamara <tamara.erlij@gmail.com>
2020-05-13 01:39:25 -0300

    As infos da API estao aparecendo (print)

---
+++ API/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  API
+//
+//  Created by Tamara Erlij on 13/05/20.
+//  Copyright © 2020 Tamara Erlij. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=439d4b804bc8187953eb36d2a8c26a02")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print(error)
+            } else {
+                if let urlContent = data{
+                    
+                    do  {
+                    let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as  AnyObject
+                        print(jsonResult)
+                        print(jsonResult["name"])
+                    } catch {
+                        print("JSON Processing Failed")
+                    }
+                
+            }
+        }
+    }
+        task.resume()
+    }
+}
+
